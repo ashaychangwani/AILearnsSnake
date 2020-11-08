0334a73d6d86fa40ba30c8cc39c6d516f3d960dd
Ashay Changwani <ashaychangwani@gmail.com>
2020-11-08 20:28:56 +0530

    Integrated boundary.py

--- boundary.py
+++ boundary.py
@@ -2,62 +2,42 @@ import pygame
 import random
 
 
-class Playground:
-    def __init__(self, height, width, block_size):
+class Environment:
+    def __init__(self, height, width, unit_size):
         self.height = height
         self.width = width
-        self.block_size = block_size
-        self.food = (0, 0)
+        self.unit = unit_size
+        self.apple_position = (0, 0)
 
-    def create(self, playground, background_color, color):
-        playground.fill(background_color)
+    def draw_apple(self, environment, color):
+        apple_image = pygame.image.load('apple.png')
+        apple_image = pygame.transform.scale(apple_image, (10, 10))
+        environment.blit(
+            apple_image, (self.apple_position[0], self.apple_position[1], self.unit, self.unit))
+        return environment
 
-        bs = self.block_size
+    def draw_boundary(self, environment, color):
+        unit = self.unit
+        for w in range(0, self.width, self.unit):
+            pygame.draw.rect(environment, color, (w, 0, unit, unit))
+            pygame.draw.rect(environment, color,
+                             (w, self.height - unit, unit, unit))
+        for h in range(0, self.height, self.unit):
+            pygame.draw.rect(environment, color, (0, h, unit, unit))
+            pygame.draw.rect(environment, color,
+                             (self.width - unit, h, unit, unit))
 
-        for w in range(0, self.width, bs):
-            y = 0
-
-            pygame.draw.rect(playground, color, (w, y, bs, bs), 1)
-            pygame.draw.rect(playground, color, (w + 3, y+3, bs-6, bs-6))
-
-            y = self.height - y
-
-            pygame.draw.rect(playground, color, (w, y, bs, bs), 1)
-            pygame.draw.rect(playground, color, (w+3, y+3, bs-6, bs-6))
-
-        for h in range(0, self.height, bs):
-            x = 0
-
-            pygame.draw.rect(playground, color, (x, h, bs, bs), 1)
-            pygame.draw.rect(playground, color, (x+3, h+3, bs-6, bs-6), 1)
-
-            x = self.width - x
-
-            pygame.draw.rect(playground, color, (x, h, bs, bs), 1)
-            pygame.draw.rect(playground, color, (x+3, h+3, bs-6, bs-6))
-        return playground
+    def create(self, environment, color):
+        environment.fill((200, 200, 200))
+        self.draw_boundary(environment, color)
+        return environment
 
     def create_new_apple(self, snake_position):
-        found = False
-        bs = self.block_size
-        while not found:
-            x = random.randint( 2*bs, self.width -2*bs)
-            x = x - (x%bs)
-            y= random.randint( 2*bs, self.height -2*bs)
-            y = y - (y%bs)
-            
-            i= 0 
-            while i<len(snake_position):
-                if x == snake_position[i][0] and y == snake_position[i][1]:
-                    break
-                i+=1
-            if i == len(snake_position):
-                found = True
-                
-        self.food = (x, y)
-        return self.food
-
-    def draw_apple(self, playground, color):
-        pygame.draw.rect(playground, color, (self.food[0], self.food[1], self.block_size, self.block_size))
-        return playground 
-
+        unit = self.unit
+        apple_position = (unit*random.randint(2, self.width/unit - 2),
+                          unit*random.randint(2, self.height/unit - 2))
+        while any(body == apple_position for body in snake_position):
+            apple_position = (unit*random.randint(2, self.width/unit - 2),
+                              unit*random.randint(2, self.height/unit - 2))
+        self.apple_position = apple_position
+        return self.apple_position
