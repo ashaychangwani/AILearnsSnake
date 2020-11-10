d97e5e5a6e733d1ee298cfa41dd66a512d7952a2
Ashay Changwani <ashaychangwani@gmail.com>
2020-11-10 17:35:52 +0530

    snake integration complete, even faster exec

--- snake.py
+++ snake.py
@@ -4,189 +4,100 @@ import random
 
 
 class snake:
-    def __init__(self, width, height, layers, block_size, random_params=True, random_start=True):
+    def __init__(self, width, height, NN_shape, unit, init_NN=True, random_start=True):
         self.snake_position = []
         self.width = width
         self.height = height
-        self.steps_taken = 0
-        self.no_of_same_result = 0
+        self.time_since_apple = 0
         self.collision_with_boundary = False
         self.collision_with_self = False
-        self.block_size = block_size
+        self.unit = unit
 
         self.neuralnet = NeuralNet(
-            layers, self.width, self.height, self.block_size, random_params)
+            NN_shape, self.width, self.height, self.unit, init_NN)
 
+        self.snake_position.append(self.initSnake(random_start))
+
+    def initSnake(self, random_start):
         if random_start:
-            self.direction = random.choice(['east', 'north', 'south', 'west'])
-            x = random.randint(3*block_size, width - 3*block_size)
-            self.head_x = x - (x % block_size)
-            y = random.randint(3*block_size, height - 3*block_size)
-            self.head_y = y - (y % block_size)
+            self.direction = random.choice(['RIGHT', 'UP', 'DOWN', 'LEFT'])
+            self.head_x = random.randint(
+                3,  self.width / self.unit - 3) * self.unit
+            self.head_y = random.randint(
+                3,  self.height / self.unit - 3) * self.unit
         else:
-            self.direction = 'east'
+            self.direction = 'RIGHT'
             self.head_x, self.head_y = 40, 40
-        self.snake_position.append((self.head_x, self.head_y))
-
-    def draw_snake(self, playground, color):
-        l = self.block_size
-        for (x, y) in self.snake_position:
-            draw.rect(playground, color, (x, y, l, l), 1)
-            draw.rect(playground, color, (x+3, y+3, l-6, l-6))
-        return playground
+        return (self.head_x, self.head_y)
 
     def isAlive(self):
         if not self.collision_with_self and not self.collision_with_boundary:
             return True
         return False
+    
+    def eatApple(self, direction):
+        self.snake_position.insert(0, (self.head_x, self.head_y))
+        self.move(direction)
 
-    def check_north(self):
-        if self.head_y - self.block_size < self.block_size:
-            self.collision_with_boundary = True
-        for i in range(len(self.snake_position) - 1):
-            if self.snake_position[i][0] == self.head_x and self.snake_position[i][1] == (self.head_y - self.block_size):
-                self.collision_with_self = True
-
-    # move the snake in north
-    def move_north(self):
-        self.check_north()
-        if not (self.collision_with_boundary or self.collision_with_self):
-            self.direction = 'north'
-            self.head_y = self.head_y - self.block_size
-            self.snake_position.insert(0, (self.head_x, self.head_y))
-            self.snake_position.pop()
-
-    # sets the collision_with_boundary and collision_with_self if unable to go south accordingly
-    def check_south(self):
-        if self.head_y + self.block_size >= self.height - self.block_size:
-            self.collision_with_boundary = True
-        for i in range(len(self.snake_position) - 1):
-            if self.snake_position[i][0] == self.head_x and self.snake_position[i][1] == (self.head_y + self.block_size):
-                self.collision_with_self = True
-
-    # move the snake in south
-    def move_south(self):
-        self.check_south()
-        if not (self.collision_with_boundary or self.collision_with_self):
-            self.direction = 'south'
-            self.head_y = self.head_y + self.block_size
-            self.snake_position.insert(0, (self.head_x, self.head_y))
-            self.snake_position.pop()
-
-    # sets the collision_with_boundary and collision_with_self if unable to go east accordingly
-    def check_east(self):
-        if self.head_x + self.block_size >= self.width - self.block_size:
-            self.collision_with_boundary = True
-        for i in range(len(self.snake_position) - 1):
-            if self.snake_position[i][0] == (self.head_x + self.block_size) and self.snake_position[i][1] == self.head_y:
-                self.collision_with_self = True
-
-    # move the snake in east
-    def move_east(self):
-        self.check_east()
-        if not (self.collision_with_boundary or self.collision_with_self):
-            self.direction = 'east'
-            self.head_x = self.head_x + self.block_size
-            self.snake_position.insert(0, (self.head_x, self.head_y))
-            self.snake_position.pop()
-
-    # sets the collision_with_boundary and collision_with_self if unable to go west accordingly
-    def check_west(self):
-        if self.head_x - self.block_size < self.block_size:
-            self.collision_with_boundary = True
-        for i in range(len(self.snake_position) - 1):
-            if self.snake_position[i][0] == (self.head_x - self.block_size) and self.snake_position[i][1] == self.head_y:
-                self.collision_with_self = True
-
-    # move the snake in west
-    def move_west(self):
-        self.check_west()
-        if not (self.collision_with_boundary or self.collision_with_self):
-            self.direction = 'west'
-            self.head_x = self.head_x - self.block_size
-            self.snake_position.insert(0, (self.head_x, self.head_y))
-            self.snake_position.pop()
-
-    def getNextPos(self, result):
-        l = self.block_size
-        x = self.head_x
-        y = self.head_y
-        direction = self.direction
-        if direction == 'north':
-            if result == 1:
-                return (x, y - l), 'north'
-            elif result == 2:
-                return (x - l, y), 'west'
-            else:
-                return (x + l, y), 'east'
-        elif direction == 'east':
-            if result == 1:
-                return (x + l, y), 'east'
-            elif result == 2:
-                return (x, y - l), 'north'
-            else:
-                return (x, y + l), 'south'
-        elif direction == 'south':
-            if result == 1:
-                return (x, y + l), 'south'
-            elif result == 2:
-                return (x + l, y), 'east'
-            else:
-                return (x - l, y), 'west'
+    def moveInDirection(self, direction):
+        if direction == 'UP':
+            self.head_y = self.head_y - self.unit
+        elif direction == 'DOWN':
+            self.head_y = self.head_y + self.unit
+        elif direction == 'LEFT':
+            self.head_x = self.head_x - self.unit
         else:
-            if result == 1:
-                return (x - l, y), 'west'
-            elif result == 2:
-                return (x, y + l), 'south'
-            else:
-                return (x, y - l), 'north'
+            self.head_x = self.head_x + self.unit
+        self.direction = direction
+        self.snake_position.insert(0, (self.head_x, self.head_y))
+        self.snake_position.pop()
+        self.check_valid()
 
-    def check_collision_with_self(self, x, y):
-        for i in range(3, len(self.snake_position)-1):
-            if self.snake_position[i][0] == x and self.snake_position[i][1] == y:
-                return True
-        return False
-    
-    def updateSnakePosition(self,result):
-        pos, dir = self.getNextPos(result)
-        if(pos[0] != 0) and (pos[0] != self.width - self.block_size) and (pos[1] != 0) and (pos[1] != self.height - self.block_size) and (not self.check_collision_with_self(pos[0], pos[1])):
-            self.head_x, self.head_y = pos[0], pos[1]
-            self.snake_position.insert(0, (self.head_x, self.head_y))
-            self.direction = dir 
-            return True
-        return False
-    
+    def check_valid(self):
+        if self.head_x == self.unit or self.head_x == self.width - self.unit or self.head_y == self.unit or self.head_y == self.height - self.unit:
+            self.collision_with_boundary = True
+        for (body_x, body_y) in self.snake_position[1:]:
+            if body_x == self.head_x and body_y == self.head_y:
+                self.collision_with_self = True
+
     def move(self, result):
-        if self.direction == 'north':
+        if self.direction == 'UP':
             if result == 1:
-                self.move_north()
+                self.moveInDirection('UP')
             elif result == 2:
-                self.move_west()
+                self.moveInDirection('LEFT')
             else:
-                self.move_east()
-        elif self.direction == 'east':
+                self.moveInDirection('RIGHT')
+        elif self.direction == 'RIGHT':
             if result == 1:
-                self.move_east()
+                self.moveInDirection('RIGHT')
             elif result == 2:
-                self.move_north()
+                self.moveInDirection('UP')
             else:
-                self.move_south()
-        elif self.direction == 'south':
+                self.moveInDirection('DOWN')
+        elif self.direction == 'DOWN':
             if result == 1:
-                self.move_south()
+                self.moveInDirection('DOWN')
             elif result == 2:
-                self.move_east()
+                self.moveInDirection('RIGHT')
             else:
-                self.move_west()
+                self.moveInDirection('LEFT')
         else:
             if result == 1:
-                self.move_west()
+                self.moveInDirection('LEFT')
             elif result == 2:
-                self.move_south()
+                self.moveInDirection('DOWN')
             else:
-                self.move_north()
-        self.steps_taken += 1
+                self.moveInDirection('UP')
+        self.time_since_apple += 1
         return self.isAlive()
-    
-            
-    
+
+    def draw_snake(self, environment, color, color_head):
+        l = self.unit
+        draw.rect(environment, color_head, (self.head_x, self.head_y, l, l), 1)
+        draw.rect(environment, color_head,
+                  (self.head_x+2, self.head_y+2, l-4, l-4))
+        for (x, y) in self.snake_position[1:]:
+            draw.rect(environment, color, (x, y, l, l), 1)
+            draw.rect(environment, color, (x+2, y+2, l-4, l-4))
+        return environment
